f287b8fff4ee3ef3593f4a3245c8a5d966982ecf
uiuxsooa <sooa0513@naver.com>
2020-01-28 23:55:30 +0900

    Updates

---
+++ precache-manifest.454f938cb0e6cdb19a8115de081d137a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90e84c49508b3e3914eeed85cc5a0e9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "3750361070cac5315237",
+    "url": "/movie_app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "235a359e62b6fa7e0c64",
+    "url": "/movie_app/static/js/2.9010781d.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app/static/js/2.9010781d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3750361070cac5315237",
+    "url": "/movie_app/static/js/main.f603e450.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
